a6628427110e382f28e49ab793393d2624ed9077
Daniel Berezansky <dbere@uw.edu>
2022-11-26 22:11:30 -0800

    Standard distribution model

--- src/DistributionModel.h
+++ src/DistributionModel.h
@@ -6,3 +6,10 @@ class DistributionModel {
     public:
         virtual tuple<int, int, int> getWeights(int batchSize, tuple<int, int, int> sizes) = 0;
 };
+
+class Standard : public DistributionModel {
+    public:
+        tuple<int, int, int> getWeights(int batchSize, tuple<int, int, int> sizes) {
+            return tuple(80, 10, 10);
+        }
+};
