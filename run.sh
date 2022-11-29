c100c8ea8c8e3732ef15640d4fe0fbe6a7e0b9f0
Amit Ferman <aferman@uw.edu>
2022-11-28 20:10:01 -0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 
-make
+if [ $# -ne 0 ]; then
+    echo "Usage: No Arguments"
+    exit
+fi
 
+echo "Preparing..."
+make > /dev/null 2>&1
+echo "Executing Quartz"
 ./bin/main -r 5.0 -lw1 0.7 -lw2 0.2 -lw3 0.1
